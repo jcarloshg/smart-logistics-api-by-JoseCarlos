685c9f33e77cd44768d9ab74ddf5ab308a3689a7
jcarloshg <carlosj12336@gmail.com>
2026-02-27 18:32:52 -0600

    update schema to create data bse

--- database/migrations/2026-02-27.main-schema.sql
+++ database/migrations/2026-02-27.main-schema.sql
@@ -1,14 +1,9 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- Create node table
+-- Create graph table
 CREATE TABLE IF NOT EXISTS node (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
-    name VARCHAR(255) NOT NULL,
-    axis_x NUMERIC NOT NULL,
-    axis_y NUMERIC NOT NULL,
+    graph JSON NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
--- Create index on email for faster lookups
--- CREATE INDEX IF NOT EXISTS idx_node_email ON node (email);
