fea95c5fbbd6ec6a9e803527339eb3483dde5f3e
jcarloshg <carlosj12336@gmail.com>
2026-03-01 14:34:19 -0600

    upadte preference for DTO for optimiza flow

--- smart_api/src/application/optimize_route/models/optimize_route.response.entity.ts
+++ smart_api/src/application/optimize_route/models/optimize_route.response.entity.ts
@@ -1,6 +1,9 @@
+import { Preference } from "./optimize_route_request.entity";
+
 export interface OptimizeRouteResponse {
     graphId: string;
     totalCost: number;
     path: string[];
     durationMs?: number;
+    preference?: Preference;
 }
--- smart_api/src/application/optimize_route/models/optimize_route_request.entity.ts
+++ smart_api/src/application/optimize_route/models/optimize_route_request.entity.ts
@@ -3,6 +3,8 @@ import { z } from 'zod';
 export const OptimizeRouteSchema = z.object({
     originNodeId: z.string().min(1, 'originNodeId is required'),
     destinationNodeId: z.string().min(1, 'destinationNodeId is required'),
+    preference: z.enum(['shortest', 'fastest']).default('shortest'),
 });
 
 export type OptimizeRouteRequest = z.infer<typeof OptimizeRouteSchema>;
+export type Preference = 'shortest' | 'fastest';
